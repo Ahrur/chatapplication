0325919592f6b17dfd4f4cc188612a2c842b9d16
Ahrur <67335893+Ahrur@users.noreply.github.com>
2020-06-24 20:34:09 +0530

    Update server.java

--- server.java
+++ server.java
@@ -15,24 +15,32 @@ import java.net.*;
             String msgin = "",msgout ="";
 
             while(!msgin.equals("end")){
+                String msginen = new String();
+                String msgouten = new String();
+
                 msgin = din.readUTF();
-                char[] msginen = msgout.toCharArray();
-                for(char c : msginen){
-                    c-=5;
+                char c;
+
+                for(int i=0; i<msgin.length(); i++){
+                    c = msgin.charAt(i);
+                    c = (char) ((int)c+1);
+                    msginen += Character.toString(c);
                 }
-                System.out.println(msgin);
+                System.out.println("Client: "+msginen);
 
                 msgout = br.readLine();
-                char[] msgouten = msgout.toCharArray();
-                for(char c : msgouten){
-                    c+=5;
+                char e;
+                for(int j=0; j<msgout.length(); j++){
+                    e = msgout.charAt(j);
+                    e = (char) ((int)e-1);
+                    msgouten += Character.toString(e);
                 }
-                dout.writeUTF(msgout);
+                dout.writeUTF(msgouten);
                 dout.flush();
             }
             s.close();
         } catch (IOException e) {
-            e.printStackTrace();
+           e.printStackTrace();
         }
     }
 }
